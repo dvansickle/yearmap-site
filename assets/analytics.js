44a55752395819f47f476fefb2684765d7bf870a
David Van Sickle <dvansickle@gmail.com>
2026-06-30 11:15:19 -0500

    Use official PostHog loader

--- assets/analytics.js
+++ assets/analytics.js
@@ -2,56 +2,7 @@
   const POSTHOG_TOKEN = 'phc_pUqH8ixDgGpn2HLugzCheC6PXPNTFvFy8abTvkdjuxgn';
   const POSTHOG_HOST = 'https://us.i.posthog.com';
 
-  function loadPostHog(t, e) {
-    let o;
-    let n;
-    let p;
-    let r;
-    if (e.__SV || (window.posthog && window.posthog.__loaded)) return;
-    window.posthog = e;
-    e._i = [];
-    e.init = function init(i, s, a) {
-      function g(t, e) {
-        const o = e.split('.');
-        if (o.length === 2) {
-          t = t[o[0]];
-          e = o[1];
-        }
-        t[e] = function call() {
-          t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
-        };
-      }
-      p = t.createElement('script');
-      p.type = 'text/javascript';
-      p.crossOrigin = 'anonymous';
-      p.async = true;
-      p.src = s.api_host.replace('.i.posthog.com', '-assets.i.posthog.com') + '/static/array.js';
-      r = t.getElementsByTagName('script')[0];
-      r.parentNode.insertBefore(p, r);
-      let u = e;
-      if (a !== undefined) {
-        u = e[a] = [];
-      } else {
-        a = 'posthog';
-      }
-      u.people = u.people || [];
-      u.toString = function toString(t) {
-        let e = 'posthog';
-        if (a !== 'posthog') e += `.${a}`;
-        if (!t) e += ' (stub)';
-        return e;
-      };
-      u.people.toString = function peopleToString() {
-        return `${u.toString(1)}.people (stub)`;
-      };
-      o = 'capture register register_once register_for_session unregister unregister_for_session get_distinct_id get_session_id reset set_config opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing'.split(' ');
-      for (n = 0; n < o.length; n += 1) g(u, o[n]);
-      e._i.push([i, s, a]);
-    };
-    e.__SV = 1;
-  }
-
-  loadPostHog(document, window.posthog || []);
+  !function(t,e){var o,n,p,r;e.__SV||(window.posthog && window.posthog.__loaded)||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="ki Ci init qi Hi pr ji zi Di capture calculateEventProperties Qi register register_once register_for_session unregister unregister_for_session Ki getFeatureFlag getFeatureFlagPayload getFeatureFlagResult getAllFeatureFlags isFeatureEnabled reloadFeatureFlags updateFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey displaySurvey cancelPendingSurvey canRenderSurvey canRenderSurveyAsync Xi identify setPersonProperties unsetPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags reset setIdentity clearIdentity get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException addExceptionStep captureLog startExceptionAutocapture stopExceptionAutocapture loadToolbar get_property getSessionProperty Ji Gi createPersonProfile setInternalOrTestUser Yi Ai rn opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Vi debug mr it getPageViewId captureTraceFeedback captureTraceMetric Oi".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
 
   posthog.init(POSTHOG_TOKEN, {
     api_host: POSTHOG_HOST,
